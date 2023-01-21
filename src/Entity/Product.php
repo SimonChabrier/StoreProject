74f65f78412d7c0ae9cbfc4c1bda255ea1118731
Simon Chabrier <simonchabrier@gmail.com>
2023-01-21 20:56:27 +0100

    clean

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -64,6 +64,22 @@ class Product
      */
     private $visibility = 1;
 
+    /**
+     * Permet de calculer la marge brute d'un produit
+     * n'est pas persisté en base de données
+     */
+    private $tauxMarque;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $inStock = 0;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $inStockQuantity = 0;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -216,6 +232,62 @@ class Product
     public function setVisibility(bool $visibility): self
     {
         $this->visibility = $visibility;
+
+        return $this;
+    }
+
+    public function tauxMarque(): ?string
+    {   
+        // On calcule la marge commerciale HT pour easyAdmin
+        //TODO A paufiner et vérifier la formule de calcul
+
+        // pour EasyAdmin on retourne 0 si le prix d'achat ou de vente est à 0
+        // pour ne pas avoir de division par 0 et donc une erreur à la création d'un produit
+        if ($this->buyPrice == 0 || $this->sellingPrice == 0) {
+            return '0 %';
+        }
+        
+        $achatHt = $this->buyPrice * 0.8;
+        $venteHt = $this->sellingPrice * 0.8;
+
+        $margeCommerciale = $venteHt - $achatHt;
+        $tauxDeMarge = ($margeCommerciale / $achatHt) * 100;
+        $tauxDeMarque = ($margeCommerciale / $venteHt) * 100;
+
+        $result = round($tauxDeMarge , 2) . ' %';
+        if ($result <= 25) {
+            return '🔴 ' . $this->tauxMarque = $result;
+        } elseif ($result > 25 && $result <= 40) {
+            return '🟡 ' . $this->tauxMarque = $result;
+        } elseif ($result > 40) {
+            return '🟢 ' .$this->tauxMarque = $result;
+        } else {
+            return $this->tauxMarque = 'informations incomplètes';
+        }
+
+        //return $this->tauxMarque = round($tauxDeMarque , 2) . ' %';
+    }
+
+    public function isInStock(): ?bool
+    {
+        return $this->inStock;
+    }
+
+    public function setInStock(bool $inStock): self
+    {
+        $this->inStock = $inStock;
+
+        return $this;
+    }
+
+    public function getInStockQuantity(): ?string
+    {
+        return $this->inStockQuantity;
+    }
+
+    public function setInStockQuantity(string $inStockQuantity): self
+    {
+        $this->inStockQuantity = $inStockQuantity;
 
         return $this;
     }

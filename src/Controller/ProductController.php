88c8913f4f12717ef037ee5d81bfd411e740e7d2
Simon Chabrier <simonchabrier@gmail.com>
2023-02-23 23:40:38 +0100

    add relatedProducts on product page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -50,11 +50,13 @@ class ProductController extends AbstractController
     /**
      * @Route("/{id}", name="app_product_show", methods={"GET"})
      */
-    public function show(Product $product): Response
+    public function show(Product $product, ProductRepository $pr): Response
     {   
         dump($product);
+        
         return $this->render('product/show.html.twig', [
             'product' => $product,
+            'relatedProducts' => $pr->relatedProducts($product->getSubCategory()->getId()),
         ]);
     }
 

d58f70f141bacb6846a2bbd3161c988eb74fb800
Simon Chabrier <simonchabrier@gmail.com>
2023-07-20 14:46:11 +0200

    add cart template

---
+++ src/Form/CartType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CartType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('items', CollectionType::class, [
+                'entry_type' => CartItemType::class
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Mettre à jour le panier'
+            ])
+            ->add('clear', SubmitType::class, [
+                'label' => 'Vider le panier'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}

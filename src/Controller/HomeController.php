dec3f7fcc8069f086a51a40abf324ce10dedaf0f
Simon Chabrier <simonchabrier@gmail.com>
2023-08-12 13:35:32 +0200

    optimize setProductData on home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -103,8 +103,14 @@ class HomeController extends AbstractController
                 'homeCats' => $viewData,
             ]);
     }
-
-    public function setProductData($products)
+    /**
+     * Pour chaque produit, on récupère les données dont on a besoin et on les stocke dans un tableau
+     * le cache ne peut pas stocker les objets Doctrine, il faut donc les transformer en tableau
+     * sinon on a une erreur de type : "Object of class App\Entity\Product could not be converted to string"
+     * Si on ajoute à la récumèration des données une serialization pour les mettre en cache et une deserialization pour les afficher
+     * on va consommer plus de ressources serveur pour rien, donc on ne le fait pas.
+     */
+    public function setProductData($products): array
     {    
         $productsData = [];
 

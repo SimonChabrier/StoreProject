53fd88a7fbc99fc1ad0789b4c813022f801498d2
Simon Chabrier <simonchabrier@gmail.com>
2023-08-15 11:50:48 +0200

    debug messenger

--- src/Message/UpdateFileMessage.php
+++ src/Message/UpdateFileMessage.php
@@ -8,15 +8,18 @@ class UpdateFileMessage
     private $name = 'nom par défaut';
     private $alt = 'alt par défaut';
     private $product;
+    private $tempFileName;
 
     public function __construct(
         string $name,
         string $alt,
-        $product
+        $product,
+        $tempFileName
     ) {
         $this->name = $name;
         $this->alt = $alt;
         $this->product = $product;
+        $this->tempFileName = $tempFileName;
     }
 
     public function getName(): string
@@ -30,5 +33,9 @@ class UpdateFileMessage
     public function getProduct()
     {   
         return $this->product;
+    }
+    public function getTempFileName()
+    {   
+        return $this->tempFileName;
     }
 }

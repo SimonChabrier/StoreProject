1f7ec6525dcc7b5beffd74fc69200fa0917c2154
Simon Chabrier <simonchabrier@gmail.com>
2023-08-19 22:07:05 +0200

    add a clear cache before all entity flush exclude order and order item to have a update jsonfile if product did not have picture on create or update

--- src/EventSubscriber/ClearCacheSubscriber.php
+++ src/EventSubscriber/ClearCacheSubscriber.php
@@ -11,10 +11,8 @@ use Doctrine\Common\EventSubscriber as DoctrineEventSubscriber;
 use Doctrine\ORM\Event\PostFlushEventArgs as PostFlushEventArgs;
 
 
-//! EN ATTENTE DE VOIR SI BESOIN DE REUTILISER CES EVENEMENTS OU PAS DANS LE FUTUR (ABANDONNE LE 16/AOUT/2023 POUR D'AUTRES SOLUTIONS)
-
 // Cette classe est chargée de supprimer le cache et de refaire le fichier json après chaque flush d'une entité
-// Elle est appelée à chaque fois qu'une entité est créée, modifiée ou supprimée
+// Elle est appelée à chaque fois qu'une entité est créée, modifiée ou supprimée (sauf pour les entités Order et OrderItem)
 
 class ClearCacheSubscriber implements DoctrineEventSubscriber
 {
@@ -47,47 +45,48 @@ class ClearCacheSubscriber implements DoctrineEventSubscriber
      */
     public function postFlush(PostFlushEventArgs $args)
     {   
-        //TODO en test pour voir si il faudra ou pas exclure les entités Order et OrderItem du cache
-        //TODO actuellement on refait le json et le cache à chaque flush d'une entité
+        // TODO en test pour voir si il faudra ou pas exclure les entités Order et OrderItem du cache
         // TODO pour le moment c'est le moyen le plus simple de maintenir le cache et le json à jour après chaque flush
+        // TODO car si on update ou crée un produit sans image alors le cache et le json ne sont pas mis à jour puisque le uplaodService
+        // TODO n'est pas appelé et donc le message n'est pas envoyé au worker et donc le cache et le json ne sont pas mis à jour
         
         // on récupère toutes les entités qui ont été modifiées lors du flush
-        // $entities = $args->getEntityManager()->getUnitOfWork()->getIdentityMap();
+        $entities = $args->getEntityManager()->getUnitOfWork()->getIdentityMap();
         // si le flush implique une modification d'une entité Order ou OrderItem on ne fait rien
-        // if(array_key_exists(Order::class, $entities) && array_key_exists(OrderItem::class, $entities)){
-        //     return;
-        // };
-
+        if(array_key_exists(Order::class, $entities) && array_key_exists(OrderItem::class, $entities)){
+            return;
+        };
         // Sinon on supprime le cache et on refait le json
-        // vérifier si toutes les images sont à jour avec le status 'done'
-
-       //* $this->clearCacheService->clearCacheAndJsonFile(self::CACHE_KEY);
+       $this->clearCacheService->clearCacheAndJsonFile(self::CACHE_KEY);
     }
+
+
+    //////////////////////////// EN ATTENTE DE VOIR SI BESOIN DE REUTILISER CES EVENEMENTS OU PAS DANS LE FUTUR (ABANDONNE LE 16/AOUT/2023 POUR D'AUTRES SOLUTIONS) ////////////////////////////
 
     //*  NON UTILISE CAR ON UTLISE POST FLUSH
     // met à jour le cache et le json après la création d'une entité
-    public function postPersist(LifecycleEventArgs $args)
-    {   
-        $entity = $args->getObject();
+    // public function postPersist(LifecycleEventArgs $args)
+    // {   
+    //     $entity = $args->getObject();
 
-        // Exclure les entités Order et OrderItem du cache
-        if (!$entity instanceof Order && !$entity instanceof OrderItem) {
-            // On supprime le cache et on refait le json
-            //$this->clearCacheService->clearCacheAndJsonFile(self::CACHE_KEY);
-        }
-    }
+    //     // Exclure les entités Order et OrderItem du cache
+    //     if (!$entity instanceof Order && !$entity instanceof OrderItem) {
+    //         // On supprime le cache et on refait le json
+    //         //$this->clearCacheService->clearCacheAndJsonFile(self::CACHE_KEY);
+    //     }
+    // }
 
     //*  NON UTILISE CAR ON UTLISE POST FLUSH
     // met à jour le cache après une modification d'entité
-    public function postUpdate(LifecycleEventArgs $args)
-    {   
-        $entity = $args->getObject();
+    // public function postUpdate(LifecycleEventArgs $args)
+    // {   
+    //     $entity = $args->getObject();
 
-        // Exclure les entités Order et OrderItem du cache
-        if (!$entity instanceof Order && !$entity instanceof OrderItem) {
-            // On supprime le cache et on refait le json
-            // todo pose problème si on utilise messenger il faudra utiliser le workflow ou le post persit pour créer le fichier json
-//$this->clearCacheService->clearCacheAndJsonFile(self::CACHE_KEY);
-        }
-    }
+    //     // Exclure les entités Order et OrderItem du cache
+    //     if (!$entity instanceof Order && !$entity instanceof OrderItem) {
+    //         // On supprime le cache et on refait le json
+    //         // todo pose problème si on utilise messenger il faudra utiliser le workflow ou le post persit pour créer le fichier json
+    //         $this->clearCacheService->clearCacheAndJsonFile(self::CACHE_KEY);
+    //     }
+    // }
 }

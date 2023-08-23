42098bce5ece5386d322402f7dc378e52d5f34cd
Simon Chabrier <simonchabrier@gmail.com>
2023-08-23 08:28:57 +0200

    add order info for current user in topbar

--- src/Service/GlobalVarsService.php
+++ src/Service/GlobalVarsService.php
@@ -4,19 +4,23 @@ namespace App\Service;
 
 use App\Entity\Brand;
 use App\Entity\Category;
+use App\Entity\Order;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\Security\Core\Security;
 
 class GlobalVarsService
 {
     private $em;
     private $cache;
+    private $security;
 
-    public function __construct(EntityManagerInterface $em, CacheInterface $cache)
+    public function __construct(EntityManagerInterface $em, CacheInterface $cache, Security $security)
     {
         $this->em = $em;
         $this->cache = $cache;
+        $this->security = $security;
     }
 
     // retourne toutes les catégories pour le menu de navigation
@@ -24,7 +28,7 @@ class GlobalVarsService
     // la méthode est ensuite appellée dans twig comme ceci: {% for nav in catsAndSubCats.getNavCatsAndSubCats() %} 
     // pour cible directement la méthode sur la variable globale injectée dans tous les templates twig.
 
-    public function getNavCatsAndSubCats()
+    public function getNavCatsAndSubCats() : ?array
     {   
         // $globalNavData = $this->em->getRepository(Category::class)->getOnlyCatAndOnlySubCat();
         // mettre en cache pour éviter de faire une requête à chaque fois
@@ -40,7 +44,7 @@ class GlobalVarsService
     // déclaré dans twig comme un service injecté dans une variable 'brands' rendue globale..(voir config/twig.yaml)
     // la méthode est ensuite appellée dans twig comme ceci: {% for brand in brands.getBrands() %} 
     
-    public function getBrands()
+    public function getBrands() : ?array
     {   
         //return $this->em->getRepository(Brand::class)->findBy([], ['name' => 'ASC']);
         // mettre en cache pour éviter de faire une requête à chaque fois
@@ -49,5 +53,15 @@ class GlobalVarsService
             return $this->em->getRepository(Brand::class)->findBy([], ['name' => 'ASC']);
         });
         return $brands;
+    }
+
+    public function getUserOrder() : ?int
+    {
+        $user = $this->security->getUser();
+        //$order = $this->em->getRepository(Order::class)->findOneBy(['user' => $user, 'status' => Order::STATUS_CART]);
+        //dump($order);
+        $orderCount = $this->em->getRepository(Order::class)->count(['user' => $user, 'status' => Order::STATUS_CART]);
+        //dd($orderCount);
+        return $orderCount;
     }
 }

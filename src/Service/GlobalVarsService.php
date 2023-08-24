7b56d481df104fbb616977d6f3deebb34e693b1a
Simon Chabrier <simonchabrier@gmail.com>
2023-08-24 10:11:37 +0200

    work on order templates

--- src/Service/GlobalVarsService.php
+++ src/Service/GlobalVarsService.php
@@ -9,18 +9,22 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class GlobalVarsService
 {
     private $em;
     private $cache;
     private $security;
+    private $requestStack;
 
-    public function __construct(EntityManagerInterface $em, CacheInterface $cache, Security $security)
+    public function __construct(EntityManagerInterface $em, CacheInterface $cache, Security $security, RequestStack $requestStack)
     {
         $this->em = $em;
         $this->cache = $cache;
         $this->security = $security;
+        $this->requestStack = $requestStack;
     }
 
     // retourne toutes les catégories pour le menu de navigation
@@ -28,6 +32,9 @@ class GlobalVarsService
     // la méthode est ensuite appellée dans twig comme ceci: {% for nav in catsAndSubCats.getNavCatsAndSubCats() %} 
     // pour cible directement la méthode sur la variable globale injectée dans tous les templates twig.
 
+    /**
+     * Returns the categories and subcategories for the navigation menu.
+     */
     public function getNavCatsAndSubCats() : ?array
     {   
         // $globalNavData = $this->em->getRepository(Category::class)->getOnlyCatAndOnlySubCat();
@@ -43,7 +50,10 @@ class GlobalVarsService
     // retourne toutes les marques pour les filtres de la sidebar
     // déclaré dans twig comme un service injecté dans une variable 'brands' rendue globale..(voir config/twig.yaml)
     // la méthode est ensuite appellée dans twig comme ceci: {% for brand in brands.getBrands() %} 
-    
+
+    /**
+     * Returns the brands for the sidebar filters.
+     */
     public function getBrands() : ?array
     {   
         //return $this->em->getRepository(Brand::class)->findBy([], ['name' => 'ASC']);
@@ -55,16 +65,39 @@ class GlobalVarsService
         return $brands;
     }
 
+    /**
+     * Returns the number of items in the cart for the current user.
+     */
     public function getUserOrder() : ?int
     {
         $user = $this->security->getUser();
-        if(!$user) {
+        $userIdentifier = $this->getSession()->get('user_identifier');
+        
+        dump($user);
+        dump($userIdentifier);
+
+        if(!$user && !$userIdentifier) {
             return null;
         }
-        //$order = $this->em->getRepository(Order::class)->findOneBy(['user' => $user, 'status' => Order::CART_STATUS]);
-        //dump($order);
-        $orderCount = $this->em->getRepository(Order::class)->count(['user' => $user, 'status' => Order::CART_STATUS]); // CART_STATUS === 'new'
-        //dd($orderCount);
-        return $orderCount;
+        // si je n'ai pas de user mais que j'ai un userIdentifier en session, je récupère le nombre de paniers 'new' de cet utilisateur anonyme
+        if(!$user && $userIdentifier) {
+            $orderCount = $this->em->getRepository(Order::class)->count(['userIdentifier' => $userIdentifier, 'status' => Order::CART_STATUS]); // CART_STATUS === 'new'
+            return $orderCount;
+        }
+        // si le user est connecté, je récupère le nombre de paniers 'new' de cet utilisateur
+        if($user) {
+            $orderCount = $this->em->getRepository(Order::class)->count(['user' => $user, 'status' => Order::CART_STATUS]); // CART_STATUS === 'new'
+            return $orderCount;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the session.
+     */
+    private function getSession(): SessionInterface
+    {
+        return $this->requestStack->getSession();
     }
 }
